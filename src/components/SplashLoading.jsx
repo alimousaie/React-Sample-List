f70de260955781a118a547917e42053c99046c7e
ALi Mousaie <43743025+alimousaie@users.noreply.github.com>
2019-08-27 15:58:48 +0430

    Create SplashLoading.jsx

---
+++ src/components/SplashLoading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function SplashLoading() {
+  return (
+    <div className="circ">
+      <div className="load">Loading . . . </div>
+      <div className="hands"></div>
+      <div className="body"></div>
+      <div className="head">
+        <div className="eye"></div>
+      </div>
+    </div>
+  );
+}
